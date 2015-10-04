b483e99832c21123006c4b5b46c41578a583b181
Dylan Pinn <dylan.pinn@gmail.com>
2015-10-04 15:19:57 +1100

    Add Semantic UI class names to tables. Remove margin-bottom from p tags within an li.

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,8 @@ $(document).ready(function() {
   hljs.initHighlightingOnLoad();
   $('.tabs.menu .item').tab();
 });
+
+// Add Semantic UI classes to tables.
+$(document).ready(function() {
+  $('table').addClass('ui celled table');
+});
